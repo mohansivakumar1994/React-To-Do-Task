237661339d774ac7308635f566f7f57dfecdf665
MOHANSIVAKUMAR1994 <MOHANSIVAKUMAR359@GMAIL.COM>
2024-06-05 20:32:49 +0530

    React To Do Task

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// Importing Todo Component
+import Todo from "./Components/ToDo"
+
+function App() {
+
+  return (
+    <>
+    {/* Todo Component */}
+    <Todo />
+    </>
+  )
+}
+
+export default App
